b3b6af2b37f263e210f34f3b164cb19177274ebe
Takahiro Kamisaku <80904374+Takahiro-Kamisaku@users.noreply.github.com>
2024-09-02 11:35:08 +0900

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,40 +1,59 @@
+import boto3
 import requests
 import json
+import os
 
 # TeamsのWebhook URL
-webhook_url = 'Workflow作成時に発行されたURLを指定'
+workflows_url = os.environ["workflows_url"]'Workflow作成時に発行されたURLを指定'
+groupId = os.environ["groupId"]
+channelId = os.environ["channelId"]
 
-# 送信するメッセージ
-message = {
-  "attachments": [
-    {
-      "contentType": "application/vnd.microsoft.card.adaptive",
-      "content": {
-        "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
-        "type": "AdaptiveCard",
-        "version": "1.2",
-        "body": [
-          {
-            "type": "TextBlock",
-            "text": "## Workflow経由のTeamsへの通知テストです。\n\n* 通知の詳細1です。\n* 通知の詳細2です。",
-            "wrap": True,
-            "markdown": True
-          }
-        ]
-      }
+def send_Teams(workflows_url):
+
+    headers = {
+      "Content-Type" : "application/json"
     }
-  ]
-}
 
-# POSTリクエストを送信
-response = requests.post(
-    url=webhook_url,
-    data=json.dumps(message),
-    headers={'Content-Type': 'application/json'}
-)
+    data = {
+      "attachments": [
+        {
+          "groupId": groupId,
+          "channelId": channelId,
+          "contentType": "application/vnd.microsoft.card.adaptive",
+          "content": {
+            "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+            "type": "AdaptiveCard",
+            "version": "1.2",
+            "body": [
+              {
+                "type": "TextBlock",
+                "text": "----------------ここに件名を入力----------------",
+                "size": "Large",
+              },
+              {
+                "type": "TextBlock",
+                "text": "¥n[----ハイパーリンクテキスト----](----https://learn.microsoft.com/ja-jp/power-automate/overview-adaptive-cards----)",
+              },
+              {
+                "type": "TextBlock",
+                "text": "----textを入力----",
+              },
+            ]
+          }
+        }
+      ]
+    }
 
-# レスポンスを確認
-if response.status_code == 200 or response.status_code == 202:
+
+    response = requests.post(url=webhook_url,headers=headers,data=json.dumps(data))
+    if response.status_code == 200 or response.status_code == 202:
     print("メッセージが送信されました")
-else:
+    else:
     print(f"エラーが発生しました: {response.status_code}, {response.text}")
+
+def lambda_handler(event, context);
+    
+    #------Lambda処理を記載-------
+
+    #------処理が完了したら通知-----
+    send_Teams(workflows_url)
